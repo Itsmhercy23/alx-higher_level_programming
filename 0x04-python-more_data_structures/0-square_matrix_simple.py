73708650ab616b8bd71642729b6b92199b2e60f8
itsMhercy <mercyakinwumi36@gmail.com>
2022-06-08 16:21:59 +0100

    push

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,10 +6,11 @@ def square_matrix_simple(matrix=[]):
     # from map return new matrix
     
     new_matrix = {}
+    n = len(matrix)
 
     for idx, row in enumerate(matrix):        
         new_matrix[idx] =[]
         for col in row:
             new_matrix[idx].append(col*col)
 
-    return [new_matrix[i] for i in range(0,idx)]
+    return [new_matrix[i] for i in range(0,n)]
